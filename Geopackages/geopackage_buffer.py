d96ba939a75b7afc7bfa5ea1e4848b08b001aa3d
adg001 <39476742+adg001@users.noreply.github.com>
2020-02-25 13:09:33 +0530

    Create geopackage_buffer.py

---
+++ Geopackages/geopackage_buffer.py
@@ -0,0 +1,42 @@
+""" Import data libraries """
+from qgis import core
+from PyQt5.QtWidgets import *
+from qgis.utils import iface
+from qgis.core import QgsProject
+import processing
+
+""" Task 1. Adding Geopackae as Layers into QGIS """
+
+
+""" 1.2. Creating a Dialog Box to ask for User Input on File to Add """
+envPath = QFileDialog.getOpenFileName(QFileDialog(), "Environment Layer Select", "/home/abhishek/Desktop/M.Sc-GIS/data")[0]
+
+""" 1.3. Adding Vector Layers into QGIS """
+env = iface.addVectorLayer(envPath, "", "ogr")
+env.setName("Environment")
+
+atbnPath = QFileDialog.getOpenFileName(QFileDialog(), "Autobahn Layer Select", "/home/abhishek/Desktop/M.Sc-GIS/data")[0]
+atbn = iface.addVectorLayer(atbnPath, "", "ogr")
+atbn.setName("Autobahn")
+
+""" Task 2. Adding Buffers to Autobahn Layer """
+
+
+# 2.3. Creating a Buffer with a Standalone Script 
+
+param = { 'INPUT' : atbn, 'DISTANCE' : 20, 'SEGMENTS' : 5, 'END_CAP_STYLE' : 0, 'JOIN_STYLE' : 0, 'MITER_LIMIT' : 2, 'DISSOLVE' : False, 'OUTPUT' : 'memory:' }
+algoOutput = processing.run("qgis:buffer", param)
+autobahn20 = QgsProject.instance().addMapLayer(algoOutput['OUTPUT'])
+autobahn20.setName("Autobahn 20")
+
+
+""" 2.4. Creating 2 more Buffers """
+param = { 'INPUT' : autobahn20, 'DISTANCE' : 100, 'SEGMENTS' : 5, 'END_CAP_STYLE' : 0, 'JOIN_STYLE' : 0, 'MITER_LIMIT' : 2, 'DISSOLVE' : False, 'OUTPUT' : 'memory:' }
+algoOutput = processing.run("qgis:buffer", param)
+autobahn100 = QgsProject.instance().addMapLayer(algoOutput['OUTPUT'])
+autobahn100.setName("Autobahn 100")
+
+param = { 'INPUT' : autobahn100, 'DISTANCE' : 200, 'SEGMENTS' : 5, 'END_CAP_STYLE' : 0, 'JOIN_STYLE' : 0, 'MITER_LIMIT' : 2, 'DISSOLVE' : False, 'OUTPUT' : 'memory:' }
+algoOutput = processing.run("qgis:buffer", param)
+autobahn300 = QgsProject.instance().addMapLayer(algoOutput['OUTPUT'])
+autobahn300.setName("Autobahn 300")
